0455ec76f4aca880ebf14c67906d9913f25c63df
mihirparshy <mihir.parshy@hotmail.com>
2018-09-17 15:08:26 -0400

    created router

--- gt-athletics/src/app/app.module.ts
+++ gt-athletics/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app.routing-module';
 import { AppComponent } from './app.component';
 import { PolicyComponent } from './policy/policy.component';
 import { QuillModule } from 'ngx-quill';
@@ -15,6 +15,7 @@ import { QuillModule } from 'ngx-quill';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     QuillModule
   ],
   providers: [],
