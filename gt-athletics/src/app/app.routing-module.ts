5654de2eae35c7bc24ea6d85f7921b6f625ce286
mihirparshy <mihir.parshy@hotmail.com>
2018-09-19 14:34:22 -0400

    configured router and navbar to work with search and home components

--- gt-athletics/src/app/app.routing-module.ts
+++ gt-athletics/src/app/app.routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PolicyComponent } from './policy/policy.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
     { path: 'policy', component: PolicyComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
