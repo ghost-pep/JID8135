6af05288d611c7025a7fdfa850a1dc6bf23a8b24
Daniel Pagan <dpagan@gatech.edu>
2018-09-21 15:16:07 -0400

    successful connection

--- server/server.js
+++ server/server.js
@@ -10,34 +10,56 @@ var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var fs		   = require('fs');
 
-// configure ap to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var con_str = process.env.MONGOCON;
 
-var port = 4444;        // set our port
+// connect mongoose
+mongoose.connect(con_str);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	// we're connected!
+	console.log('connected!');
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+	// instantiate the schemas
+	var rawPolicySchema = new mongoose.Schema({
+		title: String,
+		content: String
+	});
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+	//instantiate mongoose-gridfs
+	//var gridfs = require('mongoose-gridfs')({
+	//	mongooseConnection: mongoose.connection
+	//});
+
+	// configure ap to use bodyParser()
+	// this will let us get the data from a POST
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	var port = 4444;        // set our port
+
+	// ROUTES FOR OUR API
+	// =============================================================================
+	var router = express.Router();              // get an instance of the express Router
+
+	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+	router.get('/', function(req, res) {
+		res.json({ message: 'hooray! welcome to our api!' });   
+	});
+
+	//router.get('/policy/add'......
+	//router.get('/policy/delete'......
+	//router.get('/policy/download'......
+
+	// more routes for our API will happen here
+
+	// REGISTER OUR ROUTES -------------------------------
+	// all of our routes will be prefixed with /api
+	app.use('/api', router);
+
+	// START THE SERVER
+	// =============================================================================
+	app.listen(port);
+	console.log('Magic happens on port ' + port);
+
 });
-
-//router.get('/policy/add'......
-//router.get('/policy/delete'......
-//router.get('/policy/download'......
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
-
